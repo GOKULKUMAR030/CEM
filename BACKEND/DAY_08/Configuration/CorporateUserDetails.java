de067647547e73e589e809de26009a8f125ea2e3
GOKULKUMAR030 <114055330+GOKULKUMAR030@users.noreply.github.com>
2024-02-29 16:18:17 +0530

    Create CorporateUserDetails.java

---
+++ BACKEND/DAY_08/Configuration/CorporateUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.adp.Configurtion;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.adp.Model.CorporateUser;
+
+public class CorporateUserDetails implements UserDetails {
+
+    private String name;
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+    public CorporateUserDetails (CorporateUser corporateUser)
+    {
+        name = corporateUser.getEmail();
+        password=corporateUser.getPassword();
+        authorities= Arrays.stream(corporateUser.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+         return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
